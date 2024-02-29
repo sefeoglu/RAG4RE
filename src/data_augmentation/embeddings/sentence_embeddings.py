7eae77d59e499a97081adb02fd40759a56b2a7f7
sefeoglu <sefikaefeoglu@gmail.com>
2024-02-29 19:45:41 +0100

    working still

--- src/data_augmentation/embeddings/sentence_embeddings.py
+++ src/data_augmentation/embeddings/sentence_embeddings.py
@@ -1,17 +1,30 @@
 """ This script is used to compute the sentence embeddings for the sentences in the dataset."""
 """Created by: Sefika"""
+import os
+import sys
 import json
 from sentence_transformers import SentenceTransformer
 import numpy as np
 import configparser
 
-
+PACKAGE_PARENT = '..'
+SCRIPT_DIR = os.path.dirname(os.path.realpath(os.path.join(os.getcwd(), os.path.expanduser(__file__))))
+sys.path.append(os.path.normpath(os.path.join(SCRIPT_DIR, PACKAGE_PARENT)))
+PREFIX_PATH = "/".join(os.path.dirname(os.path.abspath(__file__)).split("/")[:-2]) + "/"
 
 def read_json(path):
     with open(path, 'r') as f:
         data = json.load(f)
     return data
 
+def write_json(path, data):
+
+    if not os.path.exists(os.path.dirname(path)):
+        os.makedirs(os.path.dirname(path))
+        
+    with open(path, 'w', encoding="utf-8") as f:
+        json.dump(data, f, indent=4, ensure_ascii=False)
+        
 def compute_sentence(data):
     sent_embeddings = []
     model = SentenceTransformer('all-MiniLM-L6-v2')
@@ -36,12 +49,15 @@ def clean_sentence(sent):
 def write_embeddings(embeddings, output_file):
     np.save(output_file, embeddings)
 
-# if __name__ == "__main__":
+if __name__ == "__main__":
 
-#     config = configparser.ConfigParser()
-#     config = config.read("/Users/sefika/LLM-relation-extraction/config.ini")
-#     input_file = config["EMBEDDING"]["input_embedding_path"]
-#     output_file = config["EMBEDDING"]["output_embedding_path"]
-#     data = read_json(input_file)
-#     embeddings = compute_sentence(data)
-#     write_embeddings(embeddings, output_file)
+    print("PREFIX_PATH", PREFIX_PATH)
+
+    config = configparser.ConfigParser()
+    config.read(PREFIX_PATH+"config.ini")
+
+    input_file = config["EMBEDDING"]["input_embedding_path"]
+    output_file = config["EMBEDDING"]["output_embedding_path"]
+    data = read_json(input_file)
+    embeddings = compute_sentence(data)
+    write_embeddings(embeddings, output_file)
--- src/data_augmentation/embeddings/sentence_sim.py
+++ src/data_augmentation/embeddings/sentence_sim.py
@@ -4,31 +4,43 @@ import json
 import numpy as np
 from numpy.linalg import norm
 
-sys.path.append(os.path.join(os.path.dirname(__file__), '..'))
+PACKAGE_PARENT = '..'
+SCRIPT_DIR = os.path.dirname(os.path.realpath(os.path.join(os.getcwd(), os.path.expanduser(__file__))))
+sys.path.append(os.path.normpath(os.path.join(SCRIPT_DIR, PACKAGE_PARENT)))
+PREFIX_PATH = "/".join(os.path.dirname(os.path.abspath(__file__)).split("/")[:-2]) + "/"
 
-from utils.utility import read_json, write_triplet_json
+
 import configparser
 
+def read_json(path):
+    with open(path, 'r') as f:
+        data = json.load(f)
+    return data
 
+def write_json(path, data):
+
+    if not os.path.exists(os.path.dirname(path)):
+        os.makedirs(os.path.dirname(path))
+        
+    with open(path, 'w', encoding="utf-8") as f:
+        json.dump(data, f, indent=4, ensure_ascii=False)
+        
 
 
 def compute_similarity(test_data, train_data, train_embeddings, test_embeddings):
-
-
     similarities = []
+
     for test_index, _ in enumerate(test_data):
         test_emb = test_embeddings[test_index]
         train_similarities = []
+
         for train_index, train_line in enumerate(train_data):
+
             train_emb = train_embeddings[train_index]
             sim = np.dot(test_emb,train_emb)/(norm(test_emb)*norm(train_emb))
-            train_sentence = train_line['sentence'] #" ".join(train_line['tokens'])
-
-            head = train_line["subject"]
-            tail = train_line["object"]
-            relation = train_line["relation"]
+            train_sentence = " ".join(train_line['tokens'])
                 
-            context = "Sentence:"+ train_sentence + "\n" + "Head: " + head + "\n" + "Tail: " + tail + "\n" + "Relation type: " + relation + "\n"
+            context =  train_sentence
             train_similarities.append({"train":train_index, "simscore": sim, "sentence":context})
 
         train_similarities = sorted(train_similarities, key=lambda x: x["simscore"], reverse=True)
@@ -67,22 +79,23 @@ def main(test_file, train_file, train_emb, test_emb, output_sim_path, dataset="s
     train_data = read_json(train_file)
     train_embeddings = np.load(train_emb)
     test_embeddings = np.load(test_emb)
+
     if dataset == "semeval":
         similarities = semeval_compute_similarity(test_data, train_data, train_embeddings, test_embeddings)
     else:
         similarities = compute_similarity(test_data, train_data, train_embeddings, test_embeddings, output_sim_path)
 
-    write_triplet_json(similarities, output_sim_path)
+    write_json(similarities, output_sim_path)
 
 
 if __name__ == "__main__":
+
     config = configparser.ConfigParser()
-    config = config.read("/Users/sefika/LLM-relation-extraction/config.ini")
+    config.read(PREFIX_PATH+"config.ini")
 
     test_file = config["SIMILARITY"]["test_file"]
     train_file = config["SIMILARITY"]["train_file"]
     train_emb = config["SIMILARITY"]["train_emb"]
     test_emb = config["SIMILARITY"]["test_emb"]
     output_sim_path = config["SIMILARITY"]["output_index"]
-    metric = config["SIMILARITY"]["metric"]
     main(test_file, train_file, train_emb, test_emb, output_sim_path)
