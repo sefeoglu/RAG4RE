7054ba2df9006af620acc58b83d2cc7df66f462e
Sefika Efeoglu <sefikaefeoglu@gmail.com>
2024-06-14 19:38:28 +0200

    Update generation.py
    the experiments were done without sampling

--- src/generation_module/generation.py
+++ src/generation_module/generation.py
@@ -58,7 +58,7 @@ class LLM(object):
         
         inputs = self.tokenizer(prompt, add_special_tokens=True, max_length=526,return_tensors="pt").input_ids.to("cuda")
         
-        outputs = self.model.generate(inputs, max_new_tokens=length,do_sample=True,top_k=50,top_p=0.9)
+        outputs = self.model.generate(inputs, max_new_tokens=length)
         
         response = self.tokenizer.batch_decode(outputs, skip_special_tokens=True)
         
